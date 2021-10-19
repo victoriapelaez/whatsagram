46dd3d817d5c1e026614efeb6b3e8f810cf78aa0
Victoria <mpelaez@cifpfbmoll.eu>
2021-10-19 21:03:58 +0200

    arreglo imagen, dia fecha, funciones

--- src/js/script.js
+++ src/js/script.js
@@ -7,8 +7,8 @@ var date = new Date();
  *recorro mi colección de letras anterior, sacando cada letra de la coleccion
  *clickar en cada una de las letras activará la funcion
  *añado en el html, en el recuadro de escritura y concateno cada letra, además creo un texto al hacerlo*/
+
 var letras = document.querySelectorAll(".letra"); 
-var letrasFuncion = document.querySelector(".tecla-funcion");
 
 letras.forEach((letra) => {
   letra.onclick = function () {
@@ -19,10 +19,10 @@ letras.forEach((letra) => {
 
 
 //funcion de todo lo q hara al enviar
-    document.getElementById("enviar").onclick = function () {
+    document.getElementById("enviar").onclick = function() {
 //añadir fecha y convertir numero mes en mes
       var meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
-      document.getElementById("recuadro-fecha").innerHTML = date.getDay() + " de " + meses[date.getMonth()] + " de " + date.getFullYear();
+      document.getElementById("recuadro-fecha").innerHTML = date.getDate() + " de " + meses[date.getMonth()] + " de " + date.getFullYear();
 
 //creacion nodo de texto con hora
       var nodo = document.createElement("div");
@@ -34,15 +34,39 @@ letras.forEach((letra) => {
       document.getElementById("recuadro").appendChild(nodo);
       document.getElementById("recuadro-escribir").value = "";
     };
-  
 
-    document.getElementById("←").onclick = function () {};
-    document.getElementById("Mayus").onclick = function () {};
-    document.getElementById("↵").onclick = function () {};
+
+    //funcion borrar todo
     document.getElementById("C").onclick = function () {
       document.getElementById("recuadro-escribir").value = ""
     };
+    //funcion borrar primera letra
+    document.getElementById("→").onclick = function () {
+      let recuadroInput= document.getElementById("recuadro-escribir").value 
+       let quitarprimera=recuadroInput.substring(1)
+       console.log(quitarprimera)
+       //document.getElementById("recuadro-escribir").value = quitarprimera
+      };
+      //funcion borrar ultima letra
+    document.getElementById("←").onclick = function () {//hay q meter aqui texto y poner un return?
+     let recuadroInput= document.getElementById("recuadro-escribir").value 
+     let quitarUltima=recuadroInput.substring(0,recuadroInput.length-1)
+     console.log(quitarUltima)
+     //document.getElementById("recuadro-escribir").value= quitarUltima
+    };
+
+    //funcion mayusculas
+    document.getElementById("Mayus").onclick = function (texto) {
+
+
+
+    };
+    //funcion borrar ultima palabra
     document.getElementById("CE").onclick = function () {};
-    document.getElementById("→").onclick = function () {};
+    let frase = document.getElementById("recuadro-escribir").value
+    var ultimaPalabra = frase.lastIndexOf(" "); 
+    frase = frase.substring(0, ultimaPalabra); 
+    console.log(frase)
+    //document.getElementById("recuadro-escribir").value = frase
   };
 });
