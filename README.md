b723cd1ce600b099288e944c5bc929c1b80549dc
Victoria <mpelaez@cifpfbmoll.eu>
2021-10-31 16:59:28 +0100

    comentar y refactorizar js, actualizar readme

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # WHATSAGRAM
-## Proyecto DIW y DWEC 
+
+## Proyecto DIW y DWEC
 
 **Proyecto que consiste en crear una pequeña aplicación de mensajería.**
+
+Esta aplicación, creada con javascript en un entorno cliente, está inspirada en whassap, con la diferencia de que no permite la comunicación con un externo aún.
+Entre todas su funciones, permite escribir palabras y emoticonos, y puedes jugar con las teclas función para borrar primera o última letra, palabras, crear saltos de línea y mayúsculas. Además, he querido jugar con distintos colores y al activar el teclado emoji, la cabecera y el area de los botones cambia de color.
+También he querido recrear el modo claro y oscuro.
+Como sorpresa final, un pequeño audio incrustado al apretar el micro, para simular el envio de un mensaje de voz!
 
 ### TODO
 
 - [x] Mardown
 - [x] Crear base HTML
-- [x] Ordenae teclado *qwerty*
+- [x] Ordenar teclado _qwerty_
 - [x] Js para escribir letra
-- [x] Js para escribir frase
+- [x] Js para escribir mensaje enviado
 - [x] Espacio
 - [x] Función C -> Borrar texto, CE -> Borrar última palabra
 - [x] Funciones borrar primer y última letra
@@ -17,26 +23,44 @@
 - [x] Hacer Mayuscula con un click y dos clicks
 - [x] Función espacio
 - [x] Función salto de línea
-- [x] CSS *flexbox*(keyboard) y *grid*(layout), Responsive-centrado
+- [x] CSS _flexbox_(keyboard) y _grid_(layout), Responsive-centrado
 - [x] CSS Colores
 - [x] CSS Botones, forma y tamaño, efectos
 - [x] Importar letra
 - [x] Iconos
-- [x] Gif
+- [x] Teclado emoticonos
 - [x] Añadir fecha del dia y hora al enviar mensaje
 - [x] Añadir audio boton micro
 - [x] Conseguir audio salga como mensaje
 - [x] Conectar proyecto en Netlify
 - [x] Mejora estilo scroll
 - [x] Completar teclado emoji
-- [] Comentar codigo
-- [] Revisar y refactorizar
-- [] Problema variable global recuadroInput
+- [x] Comentar codigo
+- [x] Revisar y refactorizar
 - [x] Problema los emojis dejaron de pintarse
-- [] hacer la release y poner en git enlace web
+- [x] hacer la release y poner en git enlace web
 - [x] cambiar estilo a algo mas colorido que me defina más
 - [x] funcion modo dia/noche
 
+#### Pequeños bugs no resueltos
+
+- El modo noche cambia el color de fondo de los mensajes ya escritos, pero si se genera uno nueva, el color de fondo pertenece al modo claro inicial
+- Intente crear una variable global para recoger el texto a enviar ya que se iba a usar en muchas funciones pero al hacerlo, el programa no funcionaba. Entiendo que es porque al final se sustituye, pero no conseguí refactorizar el código para no tener que repetir tanto algunas cosas.
+- Tuve problemas con el alto de la página, así que tuve que crear el css desde cero. Aún así no ha quedado 100% perfecto a mi parecer.
+- Una vez solucioné lo de los tamaños de la página, vi que al usar el enlace de netlify en mi móvil, no se veía bien, así que lo solucioné cambiando el mínimo de ancho de la media query. Con 900 consigo que se vea bien tanto en pc, como en el samsung galaxy s5, como en cualquier móvil.
+
 ##### Herramientas usadas externas
+
 [https://markdown.es/sintaxis-markdown/]
 [https://www.javatpoint.com/javascript-onclick-event]
+[https://lenguajecss.com/css/maquetacion-y-colocacion/flexbox/]
+[https://css-tricks.com/snippets/css/complete-guide-grid/]
+[https://lemoncode.net/lemoncode-blog/2017/9/5/introduccion-programacion-funcional-javascript]
+[https://www.digitalocean.com/community/tutorials/como-modificar-atributos-clases-y-estilos-en-el-dom-es]
+[https://www.w3schools.com/charsets/ref_emoji.asp]
+[https://www.peko-step.com/es/tool/alphachannel.html]
+[https://www.it-swarm-es.com/es/css/css-para-evitar-que-el-elemento-hijo-herede-los-estilos-principales/971827137/]
+[https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity]
+[https://www.campusmvp.es/recursos/post/como-personalizar-los-estilos-css-de-las-barras-de-scroll.aspx]
+[https://donnierock.com/2019/02/06/javascript-funciones-queryselector-y-queryselectorall/]
+[https://css-tricks.com/snippets/javascript/loop-queryselectorall-matches/]
--- src/js/script.js
+++ src/js/script.js
@@ -1,245 +1,226 @@
-let controlInfiniteMayus = false;
-let letras = document.querySelectorAll(".letra");
 
-/*selecciono todas las letras que tienen la clase letra
- *recorro mi colección de letras anterior, sacando cada letra de la coleccion
- *clickar en cada una de las letras activará la funcion
- *añado en el html, en el recuadro de escritura y concateno cada letra*/
+// variable booleana para controlar el fluyo de las Mayusculas que se mantienen activas hasta que clickas
+let controlInfiniteMayus = false
+
+// variable que recoge en un array todos los botones con clase letra para recoger todos los valores del teclado
+let letras = document.querySelectorAll(".letra")
+
+
+/* FUNCIÓN PARA ENVIAR LOS MENSAJE A SU AREA
+ * Al apretar una tecla con clase letra (las recogeremos en un array que será recorrido),
+ * se activará la funcion que añadirá y concatenará las letras a formar el mensaje.
+ * Además condicionaremos que se escriba en mayúscula hasta que se aprete otra tecla o que se ponga en minúscula 
+ * dependiendo de que clase esté activada en las mayúsculas en ese momento.*/
 
 letras.forEach((letra) => {
   letra.onclick = function () {
-    document.getElementById("recuadro-escribir").value += letra.value;
+    document.getElementById("recuadro-escribir").value += letra.value
     if (controlInfiniteMayus) {
-      return;
+      return
     }
     if (letra.classList.contains("clase-mayus")) {
       letras.forEach((letra) => {
-        letra.classList.toggle("clase-mayus");
-        letra.value = letra.value.toLowerCase();
-      });
-      return;
+        letra.classList.toggle("clase-mayus")
+        letra.value = letra.value.toLowerCase()
+      })
+      return
     }
-  };
-});
+  }
+})
 
-//funcion principal de todo lo q hara al enviar
+
+
+/* FUNCIÓN PARA ENVIAR LOS MENSAJES A SU AREA 
+ * Se declaran las variables para la fecha y hora de cada mensaje, actualizándose en cada click
+ * Además convertimos el número del mes al nombre del mes
+ * Se ha de tener en cuenta que si al escribir encuantra una salto de línea, lo sustituya por <br>
+ * También convertiremos las horas y los minutos a dos cifras en el caso que la primera sea un 0.
+*/
+
 document.getElementById("enviar").onclick = function () {
-  //estas variables las pongo aqui para que a actualice la fecha y hora en cada click, poniendolo arriba solo pillaba la hora en la que caragaba la pagina
-  let hour = new Date();
-  let date = new Date();
-  //añadir fecha y convertir numero mes en mes
-  var meses = [
-    "Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Septiembre",
-    "Octubre",
-    "Noviembre",
-    "Diciembre",
-  ];
+  let hour = new Date()
+  let date = new Date()
+  var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
   document.getElementById("recuadro-fecha").innerHTML =
-    date.getDate() +
-    " de " +
-    meses[date.getMonth()] +
-    " de " +
-    date.getFullYear();
+    date.getDate() + " de " + meses[date.getMonth()] + " de " + date.getFullYear()
 
-  let saltoLinea = document
-    .getElementById("recuadro-escribir")
-    .value.replaceAll("\n", "<br>");
+  let saltoLinea = document.getElementById("recuadro-escribir").value.replaceAll("\n", "<br>")
 
-  document.querySelector(
-    ".container-recuadros"
-  ).innerHTML += `<div class="message">
-                    <div class="texto-message">${saltoLinea + " "}
-                    </div> 
-                    <div class="recuadro-hora">${
-                      hour.getHours() +
-                      ":" +
-                      (hour.getMinutes() < 10
-                        ? `0${hour.getMinutes()}`
-                        : hour.getMinutes())
-                    }
-                    &#x2713;
-                    </div>
-                  </div>
-                  `;
-  document.getElementById("recuadro-escribir").value = "";
-};
+  document.querySelector(".container-recuadros").innerHTML += 
+    `<div class="message">
+        <div class="texto-message">${saltoLinea + " "}
+        </div> 
+        <div class="recuadro-hora">${
+          hour.getHours() + ":" + (hour.getMinutes() < 10 ? `0${hour.getMinutes()}` : hour.getMinutes())
+          }
+          &#x2713;  
+        </div>
+      </div>`
+  document.getElementById("recuadro-escribir").value = ""
+}
 
-//funcion borrar todo
+// FUNCIÓN BORRAR TODO EL MENSAJE A ENVIAR
 document.getElementById("C").onclick = function () {
-  document.getElementById("recuadro-escribir").value = "";
-};
+  document.getElementById("recuadro-escribir").value = ""
+}
 
-//funcion borrar ultima palabra
+// FUNCIÓN BORRAR LA ÚLTIMA PALABRA
 document.getElementById("CE").onclick = function () {
-  let frase = document.getElementById("recuadro-escribir").value;
-  var ultimaPalabra = frase.lastIndexOf(" ");
-  frase = frase.substring(0, ultimaPalabra);
-  document.getElementById("recuadro-escribir").value = frase;
-};
+  let frase = document.getElementById("recuadro-escribir").value
+  let ultimaPalabra = frase.lastIndexOf(" ")
+  frase = frase.substring(0, ultimaPalabra)
+  document.getElementById("recuadro-escribir").value = frase
+}
 
-//funcion borrar primera letra
+// FUNCIÓN BORRAR LA PRIMERA LETRA
 document.getElementById("→").onclick = function () {
-  let recuadroInput = document.getElementById("recuadro-escribir").value;
-  document.getElementById("recuadro-escribir").value =
-    recuadroInput.substring(1);
-};
+  let recuadroInput = document.getElementById("recuadro-escribir").value
+  document.getElementById("recuadro-escribir").value = recuadroInput.substring(1)
+}
 
-//funcion borrar ultima letra
+// FUNCIÓN BORRAR LA ÚLTIMA LETRA
 document.getElementById("←").onclick = function () {
-  let recuadroInput = document.getElementById("recuadro-escribir").value;
-  document.getElementById("recuadro-escribir").value = recuadroInput.substring(
-    0,
-    recuadroInput.length - 1
-  );
-};
+  let recuadroInput = document.getElementById("recuadro-escribir").value
+  document.getElementById("recuadro-escribir").value = recuadroInput.substring( 0, recuadroInput.length - 1)
+}
 
-//funcion saltos de linea
+// FUNCIÓN SALTO DE LÍNEA
 document.getElementById("↵").onclick = function () {
-  let recuadroInput = document.getElementById("recuadro-escribir").value;
-  document.getElementById("recuadro-escribir").value = recuadroInput + "\n";
-};
+  let recuadroInput = document.getElementById("recuadro-escribir").value
+  document.getElementById("recuadro-escribir").value = recuadroInput + "\n"
+}
 
-//funcion mayusculas
+// FUNCIÓN MAYÚSCULA MANTENIDA DOBLE CLICK
 document.getElementById("Mayus").ondblclick = function () {
-  controlInfiniteMayus = true;
+  controlInfiniteMayus = true
   letras.forEach((letra) => {
-    letra.value = letra.value.toUpperCase();
-  });
-};
+    letra.value = letra.value.toUpperCase()
+  })
+}
 
+/* FUNCIÓN PRIMERA LETRA MAYÚSCULA
+ * En este caso hay que tener en cuenta la interferencia que había con la función anterior y es por ello, el uso del booleano
+ * que controle el flujo. Al hacer un click puede ejecutar dos cosas: la primera que si estaba activada la funcion de las mayusculas
+ * mantenidas, ponga las letras en minuscula y la pare. Y si no, que active que la letra se ponga en mayuscula, pero solo lo hará un vez
+ */
 document.getElementById("Mayus").onclick = function () {
   if (controlInfiniteMayus) {
     letras.forEach((letra) => {
-      letra.value = letra.value.toLowerCase();
-    });
-    controlInfiniteMayus = false;
+      letra.value = letra.value.toLowerCase()
+    })
+    controlInfiniteMayus = false
   } else {
     letras.forEach((letra) => {
-      letra.classList.toggle("clase-mayus");
-      letra.value = letra.value.toUpperCase();
-    });
+      letra.classList.toggle("clase-mayus")
+      letra.value = letra.value.toUpperCase()
+    })
   }
-};
+}
 
+/* FUNCIÓN CAMBIO TECLADO EMOJI Y ESTILO
+ * Permite al hacer click ocultar el contenedor del teclado de letras y hacer visible el contenedor del teclado de emoticonos.
+ * Además cambia el icono de emoji/teclado y cambia los colores de la cabecera y el area de teclas según el teclado que esté
+ */
 document.getElementById("emoji").addEventListener("click", function emoji() {
-  var icono = document.getElementById("emoji");
-  icono.classList.toggle("icono-cara");
+  var icono = document.getElementById("emoji")
+  icono.classList.toggle("icono-cara")
   if (icono.classList.contains("icono-cara")) {
-    icono.setAttribute("value", "\u2328");
-    document.getElementById("area-emoji").style.display = "contents";
-    document.getElementById("teclado").style.display = "none";
-    document.getElementById("keyboard-area").style.backgroundColor =
-      "rgb(107, 86, 122)";
-    document.getElementById("header-area").style.backgroundColor =
-      "rgb(107, 86, 122)";
-    document.getElementById("enviar").style.backgroundColor =
-      "rgb(107, 86, 122)";
-    document.getElementById("boton-micro").style.backgroundColor =
-      "rgb(107, 86, 122)";
+    icono.setAttribute("value", "\u2328")
+    document.getElementById("area-emoji").style.display = "contents"
+    document.getElementById("teclado").style.display = "none"
+    document.getElementById("keyboard-area").style.backgroundColor = "rgb(107, 86, 122)"
+    document.getElementById("header-area").style.backgroundColor = "rgb(107, 86, 122)"
+    document.getElementById("enviar").style.backgroundColor = "rgb(107, 86, 122)"
+    document.getElementById("boton-micro").style.backgroundColor = "rgb(107, 86, 122)"
   } else {
-    icono.setAttribute("value", "😀");
-    document.getElementById("area-emoji").style.display = "none";
-    document.getElementById("teclado").style.display = "contents";
-    document.getElementById("keyboard-area").style.backgroundColor = "#4d7470";
-    document.getElementById("header-area").style.backgroundColor = "#4d7470";
-    document.getElementById("enviar").style.backgroundColor = "#4d7470";
-    document.getElementById("boton-micro").style.backgroundColor = "#4d7470";
+    icono.setAttribute("value", "😀")
+    document.getElementById("area-emoji").style.display = "none"
+    document.getElementById("teclado").style.display = "contents"
+    document.getElementById("keyboard-area").style.backgroundColor = "#4d7470"
+    document.getElementById("header-area").style.backgroundColor = "#4d7470"
+    document.getElementById("enviar").style.backgroundColor = "#4d7470"
+    document.getElementById("boton-micro").style.backgroundColor = "#4d7470"
   }
-});
+})
 
+/* FUNCIÓN MODO OSCURO
+ * Esta función permite cambiar a modo oscuro y modo claro.
+ * El icono bombilla cambiará de color para simular encendido o apagado y a partir de cada estado, se cambiarán
+ * los colores de la aplicación de los elementos elegidos 
+ */
 document.getElementById("bombilla").addEventListener("click", function light() {
-  let bombilla = document.getElementById("bombilla");
-  bombilla.classList.toggle("bombilla-off");
+  let bombilla = document.getElementById("bombilla")
+
+  bombilla.classList.toggle("bombilla-off")
   if (bombilla.classList.contains("bombilla-off")) {
-    bombilla.style.filter = "grayscale(1)";
-    document.getElementById("header-area").style.backgroundColor =
-      "rgb(8, 43, 40)";
-    document.getElementById("keyboard-area").style.backgroundColor =
-      "rgb(8, 43, 40)";
-    document.getElementById("space").style.backgroundColor = "#323133";
-
-    let letras = document.getElementsByClassName("letra");
-    for (let i = 0; i < letras.length; i++)
-      letras[i].style.backgroundColor = "#323133";
-
-    let teclaFuncion = document.getElementsByClassName("tecla-funcion");
-    for (let i = 0; i < teclaFuncion.length; i++)
-      teclaFuncion[i].style.backgroundColor = "#323133";
-
-    document.getElementById("messaging-area").style.backgroundImage =
-      "url(images/fondo_oscuro.jpg)";
-    document.getElementById("recuadro-escribir").style.backgroundColor =
-      "#09443e";
-  
-    document.getElementById("recuadro-escribir").style.color = "white";
-
+    bombilla.style.filter = "grayscale(1)"
+    document.getElementById("header-area").style.backgroundColor = "rgb(8, 43, 40)"
+    document.getElementById("keyboard-area").style.backgroundColor = "rgb(8, 43, 40)"
+    document.getElementById("space").style.backgroundColor = "#323133"
+    document.getElementById("messaging-area").style.backgroundImage = "url(images/fondo_oscuro.jpg)"
+    document.getElementById("recuadro-escribir").style.backgroundColor = "#09443e"
+    document.getElementById("recuadro-escribir").style.color = "white"
     document.getElementById("recuadro-fecha").style.backgroundColor= "#09443e"
     document.getElementById("recuadro-fecha").style.color= "white"
-    let recuadroEscribir = document.getElementsByClassName("message");
+
+    let letras = document.getElementsByClassName("letra")
+    for (let i = 0; i < letras.length; i++)
+      letras[i].style.backgroundColor = "#323133"
+
+    let teclaFuncion = document.getElementsByClassName("tecla-funcion")
+    for (let i = 0; i < teclaFuncion.length; i++)
+      teclaFuncion[i].style.backgroundColor = "#323133"
+
+    let recuadroEscribir = document.getElementsByClassName("message")
     for (let i = 0; i < recuadroEscribir.length; i++)
-      recuadroEscribir[i].style.backgroundColor = "#09443e";
-    recuadroEscribir[i].style.color = "white"; 
+      recuadroEscribir[i].style.backgroundColor = "#09443e"
+      recuadroEscribir[i].style.color = "white"
 
   } else {
-    bombilla.style.filter = "grayscale(0)";
-    document.getElementById("header-area").style.backgroundColor =
-      "#4d7470";
-    document.getElementById("keyboard-area").style.backgroundColor =
-      "#4d7470";
-    document.getElementById("space").style.backgroundColor = "rgb(107, 86, 122)";
-
-    let letras = document.getElementsByClassName("letra");
-    for (let i = 0; i < letras.length; i++)
-      letras[i].style.backgroundColor = "rgb(107, 86, 122)";
-
-    let teclaFuncion = document.getElementsByClassName("tecla-funcion");
-    for (let i = 0; i < teclaFuncion.length; i++)
-      teclaFuncion[i].style.backgroundColor = "rgb(107, 86, 122)";
-
-    document.getElementById("messaging-area").style.backgroundImage =
-      "url(images/fondo_translucido.png)";
-    document.getElementById("recuadro-escribir").style.backgroundColor =
-      "white";
-      
-    document.getElementById("recuadro-escribir").style.color = "rgb(107, 86, 122)";
-
+    bombilla.style.filter = "grayscale(0)"
+    document.getElementById("header-area").style.backgroundColor = "#4d7470"
+    document.getElementById("keyboard-area").style.backgroundColor = "#4d7470"
+    document.getElementById("space").style.backgroundColor = "rgb(107, 86, 122)"
+    document.getElementById("messaging-area").style.backgroundImage = "url(images/fondo_translucido.png)"
+    document.getElementById("recuadro-escribir").style.backgroundColor = "white"
+    document.getElementById("recuadro-escribir").style.color = "rgb(107, 86, 122)"
     document.getElementById("recuadro-fecha").style.backgroundColor= "white"
     document.getElementById("recuadro-fecha").style.color= "rgb(107, 86, 122)"
-    let recuadroEscribir = document.getElementsByClassName("message");
-    for (let i = 0; i < recuadroEscribir.length; i++)
-      recuadroEscribir[i].style.backgroundColor = "white";
-    recuadroEscribir[i].style.color = "#4d7470";
-  }
-});
 
-window.addEventListener("load", function () {
-  document.getElementById("boton-micro").addEventListener("click", sonarAudio);
-});
-function sonarAudio() {
-  //estas variables las pongo aqui para que a actualice la fecha y hora en cada click, poniendolo arriba solo pillaba la hora en la que caragaba la pagina
-  let hour = new Date();
-  document.querySelector(
-    ".container-recuadros"
-  ).innerHTML += `<div class="message">
-  <div class="texto-message">
-  <audio src="audio/feliz_halloween.mp3" id="audio-micro" controls></audio>
-  </div> 
-  <div class="recuadro-hora">${
-    hour.getHours() +
-    ":" +
-    (hour.getMinutes() < 10 ? `0${hour.getMinutes()}` : hour.getMinutes())
+    let letras = document.getElementsByClassName("letra")
+    for (let i = 0; i < letras.length; i++)
+      letras[i].style.backgroundColor = "rgb(107, 86, 122)"
+
+    let teclaFuncion = document.getElementsByClassName("tecla-funcion")
+    for (let i = 0; i < teclaFuncion.length; i++)
+      teclaFuncion[i].style.backgroundColor = "rgb(107, 86, 122)"
+    
+    let recuadroEscribir = document.getElementsByClassName("message")
+    for (let i = 0; i < recuadroEscribir.length; i++)
+      recuadroEscribir[i].style.backgroundColor = "white"
+    recuadroEscribir[i].style.color = "#4d7470"
   }
+})
+
+
+/* FUNCIONES MENSAJE DE AUDIO
+ * Estas dos funciones permitn imitar la salida de audio con un audio predefinido al hacer click en el micro 
+ * El audio se incrustará visualmente como un mensaje con la hora de envio y sonará*/
+window.addEventListener("load", function () {
+  document.getElementById("boton-micro").addEventListener("click", sonarAudio)
+})
+
+function sonarAudio() {
+  let hour = new Date()
+  document.querySelector(".container-recuadros").innerHTML += 
+    `<div class="message">
+      <div class="texto-message">
+         <audio src="audio/feliz_halloween.mp3" id="audio-micro" controls></audio>
+      </div> 
+      <div class="recuadro-hora">${hour.getHours() + ":" + (hour.getMinutes() < 10 ? `0${hour.getMinutes()}` : hour.getMinutes())
+      }
   &#x2713;
-  </div>
-</div>
-`;
-  document.getElementById("audio-micro").play();
+      </div>
+    </div>`
+  document.getElementById("audio-micro").play()
 }
